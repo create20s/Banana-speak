4dab5e7546273055c2232e3e99d49cb75d41a912
create20s <86128319+create20s@users.noreply.github.com>
2022-11-18 10:13:07 +0530

    minion Api updated

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 var textInput=document.querySelector("#txt-input");
 var btnTranslate=document.querySelector("#btn-translate");
 var outputTxt=document.querySelector("#output");
-console.log(textInput);
+// console.log(textInput);
+
+// var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+
+var serverURL="https://api.funtranslations.com/translate/german-accent.json"
+
+
+function getTranslationURL(text) {
+   return serverURL+ "?"+ "text" +text
+   
+}
+
+function errorHandler(error) {
+   console.log("error occured", error)
+   alert("something wrong with server try aggain leter")
+   
+}
 
 function clickEventHandler(){
-   console.log("clicked");
-outputTxt.innerText="translated "+ textInput.value;
+//    console.log("clicked");
+// outputTxt.innerText="translated "+ textInput.value;
 
-}
+ var inputText= textInput.value; // taking input
+
+ // calling server for processing
+
+ fetch(getTranslationURL(inputText))
+ .then(response => response.json())
+ .then(json => {
+
+   var translatedText= json.contents.translated;
+   outputTxt.innerText= translatedText;
+   })
+ .catch(errorHandler)
+};
 btnTranslate.addEventListener("click",clickEventHandler);
 
 

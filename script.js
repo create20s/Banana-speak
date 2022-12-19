680e081c575e32814b631b8a093e3fac2c8f9c53
create20s <86128319+create20s@users.noreply.github.com>
2022-12-20 01:18:28 +0530

    api updated

--- script.js
+++ script.js
@@ -1,46 +1,34 @@
-var textInput=document.querySelector("#txt-input");
-var btnTranslate=document.querySelector("#btn-translate");
-var outputTxt=document.querySelector("#output");
-// console.log(textInput);
 
-// var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 
-var serverURL="https://api.funtranslations.com/translate/german-accent.json"
-
-
-function getTranslationURL(text) {
-   return serverURL+ "?"+ "text" +text
-   
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
 }
 
 function errorHandler(error) {
-   console.log("error occured", error)
-   alert("something wrong with server try aggain leter")
-   
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
 }
 
-function clickEventHandler(){
-//    console.log("clicked");
-// outputTxt.innerText="translated "+ textInput.value;
 
- var inputText= textInput.value; // taking input
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
 
- // calling server for processing
-
- fetch(getTranslationURL(inputText))
- .then(response => response.json())
- .then(json => {
-
-   var translatedText= json.contents.translated;
-   outputTxt.innerText= translatedText;
-   })
- .catch(errorHandler)
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
 };
-btnTranslate.addEventListener("click",clickEventHandler);
 
-
-
-// btnTranslate.addEventListener("click", function clickEventHandler(){
-//  console.log("clicked");
-// })
+btnTranslate.addEventListener("click", clickHandler)
